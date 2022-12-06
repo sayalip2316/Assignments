de5081f1fb51c0c6952ca40506d4917bb4e220df
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2022-12-06 10:24:19 +0530

    Journey in Masai School

---
+++ masai.js
@@ -0,0 +1,7 @@
+About my journey with Masai School:-
+
+	Every day it scares, how should I compete with this.New challenges facing every day. It is difficult to survive here.
+But one thing I would like appreciate about masai school is evryone is here for us, takin care of every problem we are facing.
+Never feel like alone. Whenever feel like no I can't do this, Yogesh sir came to motivate us, boost our energy.
+Day by day I started feeling grateful to be here although there are challenges scares every day. I am loving this and I hope I would be 
+able to complete this course.
